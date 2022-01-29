7be42570f7df3fd6d207b29598299401f641cc3c
Jumas Cola <kbbyfl91@gmail.com>
2022-01-29 20:45:45 +0300

    upd copy to clip function

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,6 +19,26 @@ function shortifyQuery() {
 function copyShortUrl() {
   var shortUrl = document.getElementById('short-url').innerText
 
+  // На случай, если navigator.clipboard не работает
+  if (typeof (navigator.clipboard) == 'undefined') {
+      var textArea = document.createElement("textarea");
+      textArea.value = shortUrl;
+      textArea.style.position = "fixed";  //avoid scrolling to bottom
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+
+      try {
+          var successful = document.execCommand('copy');
+          var msg = successful ? 'successful' : 'unsuccessful';
+      } catch (err) {
+        //
+      }
+
+      document.body.removeChild(textArea)
+      return;
+  }
+
   navigator.clipboard.writeText(shortUrl).then(function() {
     /* clipboard successfully set */
   }, function() {
